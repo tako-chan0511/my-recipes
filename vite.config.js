66be19bb0be5ad9e1c345a7dce28ac53866b6e4e
Hara Keisuke <harakeisuke7@gmail.com>
2025-08-04 10:19:10 +0900

    fastAPI対応マイグレーション2

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+export default defineConfig({
+    plugins: [vue()],
+    server: {
+        proxy: {
+            // '/api'で始まるリクエストを、Vercelのローカルサーバー(通常はポート3000)に転送
+            '/api': {
+                target: 'http://localhost:8686',
+                changeOrigin: true,
+                // rewrite: (path) => path.replace(/^\/api/, '') // ←ここ追加！
+            },
+        },
+    },
+});
