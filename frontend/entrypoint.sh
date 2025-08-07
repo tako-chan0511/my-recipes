fdc7103ce21cdc9aac134fce17b664b4d23451f5
Hara Keisuke <harakeisuke7@gmail.com>
2025-08-07 16:14:45 +0900

    Final Fix: NginxのDNS名前解決を安定させる

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+# DockerのDNSリゾルバを取得
+RESOLVER=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}')
+
 # 環境変数をnginx.conf.templateに適用して、新しい設定ファイルを生成
-envsubst < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+# DockerのDNSをresolverとして設定
+envsubst '${PROXY_PASS} ${RESOLVER}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Nginxを起動
 nginx -g 'daemon off;'
