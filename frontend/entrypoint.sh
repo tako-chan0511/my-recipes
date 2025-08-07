bce310a027be806892b6a010808c630c470c9618
Hara Keisuke <harakeisuke7@gmail.com>
2025-08-07 21:54:50 +0900

    Final version of all configuration files

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# PROXY_PASS(バックエンドURL)とRENDER_DNS_RESOLVER_IP(RenderのDNS)をNginx設定に埋め込む
 envsubst '${PROXY_PASS} ${RENDER_DNS_RESOLVER_IP}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
-# Nginxを起動
 nginx -g 'daemon off;'
